e658ad3f876be945a484dba668bd90d8b698f972
yugo-ibuki <y.ibuki91@gmail.com>
2024-06-23 23:16:13 +0900

    Add new command to command.go
    A new command "ctrl + ]", which allows jumping back and forth between call origin and destination, has been added to the command list in command.go. This command enhances navigation within the code.

--- pkg/command.go
+++ pkg/command.go
@@ -34,6 +34,7 @@ func Commands() Data {
 			{Command: "m{任意のキー}", Content: "任意のキーにマークをつけることができる", Description: ""},
 			{Command: "`{任意のキー}", Content: "マークの位置に飛ぶことができる", Description: ""},
 			{Command: "ctrl + o", Content: "直前のジャンプ前の位置に戻る", Description: ""},
+			{Command: "ctrl + ]", Content: "呼び出し元にジャンプ/呼び出し先にジャンプ", Description: ""},
 		},
 		"コマンドモード": {
 			{Command: "/{word}", Content: "n で次へ進み、N で 1 つ前に戻る", Description: "検索を行う"},
