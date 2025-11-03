ce96394103da1e2cab668e76c4162b5c9842880d
yugo-ibuki <y.ibuki91@gmail.com>
2025-11-03 18:41:57 +0900

    more description for the option

--- cmd/root.go
+++ cmd/root.go
@@ -3,18 +3,47 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/charmbracelet/lipgloss"
 	"github.com/spf13/cobra"
 	"github.com/yugo-ibuki/vimable/pkg"
 )
 
+// フラグ変数
+var (
+	tableFlag string
+	listFlag  bool
+)
+
 var rootCmd = &cobra.Command{
 	Use:   "vimable",
 	Short: "You can check the vim command anytime when you forget it.",
+	Long: `vimable は Vim コマンドのチートシートを表示するツールです。
+
+使用例:
+  vimable                    - すべてのコマンドテーブルを表示
+  vimable -t "ノーマルモード"  - 指定したテーブルのみを表示
+  vimable -l                 - 利用可能なテーブル一覧を表示
+  vimable -h                 - ヘルプを表示`,
+
+	Example: `  # すべてのコマンドを表示
+  vimable
+
+  # ノーマルモードとvim-surroundのコマンドのみを表示
+  vimable -t "ノーマルモード,vim-surround"
+
+  # 利用可能なテーブル一覧を表示
+  vimable -l`,
 	Run: func(cmd *cobra.Command, args []string) {
 		run()
 	},
+}
+
+func init() {
+	// フラグの追加
+	rootCmd.Flags().StringVarP(&tableFlag, "table", "t", "", "表示するテーブルを指定します（カンマ区切りで複数指定可能）")
+	rootCmd.Flags().BoolVarP(&listFlag, "list", "l", false, "利用可能なテーブル一覧を表示します")
 }
 
 func Execute() {
@@ -24,6 +53,36 @@ func Execute() {
 func run() {
 	header := pkg.Header()
 	data := pkg.Commands()
+
+	// 利用可能なテーブル一覧を表示する
+	if listFlag {
+		fmt.Println("利用可能なテーブル一覧:")
+		for key := range data {
+			fmt.Printf("- %s\n", key)
+		}
+		os.Exit(0)
+	}
+
+	// テーブルをフィルタリングする
+	filteredData := make(pkg.Data)
+	if tableFlag != "" {
+		// カンマで区切られたテーブル名を分割
+		tables := strings.Split(tableFlag, ",")
+		for _, table := range tables {
+			table = strings.TrimSpace(table)
+			if datums, ok := data[table]; ok {
+				filteredData[table] = datums
+			} else {
+				fmt.Printf("警告: テーブル '%s' は存在しません。\n", table)
+			}
+		}
+		// フィルタリングした結果が空の場合
+		if len(filteredData) == 0 {
+			fmt.Println("指定されたテーブルが見つかりません。利用可能なテーブル一覧を表示するには --list または -l オプションを使用してください。")
+			os.Exit(1)
+		}
+		data = filteredData
+	}
 
 	// Calculate optimal column widths
 	tableInstance := pkg.NewTable()
